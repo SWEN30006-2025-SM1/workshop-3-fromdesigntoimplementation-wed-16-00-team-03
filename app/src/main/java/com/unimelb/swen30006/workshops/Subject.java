80ab3355d5c7696868735f30b819ddb412cb2b69
Vannyda Long <longv@student.unimelb.edu.au>
2025-03-26 17:59:59 +1100

    added more

--- app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -1,19 +1,27 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Subject {
     String name;
     String subjectCode;
+    ArrayList<Assignment> allAss = new ArrayList<>();
+
     public Subject(String name, String subjectCode) {
         this.name = name;
         this.subjectCode = subjectCode;
     }
     public void createAss() {
-        Assignment a = Assignment.create();
+        allAss.add(Assignment.create());
     }
 
     public void deleteAss(String name) {
-
+        for (Assignment a: allAss) {
+            if (a.getName() == name) {
+                allAss.remove(name);
+                return;
+            }
+        }
     }
 }
