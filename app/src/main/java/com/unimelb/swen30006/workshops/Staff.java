77a04a217cd80e9ffba6623f00ce067695ae8c0b
Vannyda Long <longv@student.unimelb.edu.au>
2025-03-26 17:37:24 +1100

    Created a Staff Class

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Staff.java
@@ -0,0 +1,9 @@
+package com.unimelb.swen30006.workshops;
+
+import java.util.Date;
+
+public class Staff extends Person {
+    public Staff(String address, Date dob, String name) {
+        super(address, dob, name);
+    }
+}
